7f0605379684b8741ca140deddf2feefc4efd0bd
Gaurvssris <gauravsrivastwa85@gmail.com>
2026-09-21 14:59:40 +0530

    fewchanges

--- environment/Dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "GssRG"
-    storage_account_name = "gssstrg"
-    container_name       = "gsscontainer"
-    key                  = "dev.terraform.tfstate"
-  }
-}
